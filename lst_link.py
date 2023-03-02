12e92a04215a8f5828cedda65e45a2248b9ae4cf
Madara.666 NRG <lilflow81@gmail.com>
2023-03-02 13:36:58 +0300

    first commit

---
+++ lst_link.py
@@ -0,0 +1,30 @@
+lst_main_item = ['#!/accessories/head', '#!/accessories/face', '#!/accessories/neck', 
+'#!/accessories/shoulder', '#!/accessories/front', '#!/accessories/back', 
+'#!/accessories/waist', '#!/accessories/gear', '#!/animations/animations', 
+'#!/audio/audio', '#!/avatar-animations/run', '#!/avatar-animations/walk', 
+'#!/avatar-animations/fall', '#!/avatar-animations/jump', '#!/avatar-animations/idle', 
+'#!/avatar-animations/swim', '#!/avatar-animations/climb', '#!/bottoms/pants', 
+'#!/bottoms/shorts', '#!/bottoms/skirts', '#!/bundles/body-parts', 
+'#!/classic-clothing/classic-t-shirts', '#!/classic-clothing/classic-shirts', 
+'#!/classic-clothing/classic-pants', '#!/classic-head', '#!/decals/decals', 
+'#!/emote-animations/emotes', '#!/faces/faces', '#!/hair-accessories/hair', '#!/head/heads', 
+'#!/meshparts/meshes', '#!/models/models', '#!/game-passes/passes',
+'#!/shoes/left-shoe', '#!/shoes/right-shoe']
+
+фawdawd = ['192']
+
+lst_first_item = ['#!/accessories/head', '#!/accessories/face', '#!/accessories/neck', 
+'#!/accessories/shoulder', '#!/accessories/front', '#!/accessories/back', 
+'#!/accessories/waist', '#!/accessories/gear', '#!/animations/animations', 
+'#!/audio/audio', '#!/avatar-animations/run', '#!/avatar-animations/walk', 
+'#!/avatar-animations/fall', '#!/avatar-animations/jump', '#!/avatar-animations/idle', 
+'#!/avatar-animations/swim', '#!/avatar-animations/climb', '#!/badges/badges', 
+'#!/bottoms/pants', '#!/bottoms/shorts']
+
+lst_second_item = ['#!/bottoms/skirts', '#!/bundles/body-parts', '#!/classic-clothing/classic-t-shirts', 
+'#!/classic-clothing/classic-shirts', '#!/classic-clothing/classic-pants', '#!/classic-head/classic-heads', 
+'#!/decals/decals', '#!/emote-animations/emotes', '#!/faces/faces', '#!/hair-accessories/hair', '#!/head/heads', 
+'#!/meshparts/meshes', '#!/models/models', '#!/game-passes/passes','#!/private-servers/my-private-servers', 
+'#!/private-servers/other-private-servers', '#!/shoes/left-shoe', '#!/shoes/right-shoe']
+
+db_log_pass = ["madaralol111", "3Er53er5!"]
